4a006176e1863d28bb02d0c6ec5d7914234891e4
na-pydev <nurmatov.abdurahim98@mail.ru>
2021-12-17 14:29:08 +0500

    'improvements'

--- js/database.js
+++ js/database.js
@@ -23,16 +23,26 @@ let database = {
         '2': "tipleLast2('ab') → 'ababab'"
       },
       fun_name: 'tripleLast2 (str)'
+    },
+    '4': {
+      text: "Butun sonlar massivi berilgan bo'lsa, agar 6 soni massivda birinchi yoki oxirgi element sifatida ko'rinsa, true qiymatini qaytaring. Massiv uzunligi 1 yoki undan ortiq bo'ladi.",
+      examples: {
+        '1': "firstLast6([1, 2, 6]) → true",
+        '2': "firstLast6([13, 6, 1, 2, 3]) → false"
+      },
+      fun_name: 'firstLast6 (arr)'
     }
   },
   check: {
     '1': [12, 4, 5],
     '2': ['Bob', 'Alice', 'X'],
     '3': ['Hello', 'ab', 'Hi'],
+    '4': [[1, 2, 6], [6, 1, 2, 3], [13, 6, 1, 2, 3]]
   },
   answers: {
     '1': [true, false, true],
     '2': ['Hello Bob!', 'Hello Alice!', 'Hello X!'],
     '3': ['lololo', 'ababab', 'HiHiHi'],
+    '4': [true, true, false],
   }
 }
